8ab610b7083cd3aa64a7c7a5087e9529c8322fbe
NaokiSato102 <s17c1066nq@s.chibakoudai.jp>
2019-12-28 00:25:04 +0900

    delete trash file

--- .myled.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/my-LED-task/myled.ko := ld -EL -r  -maarch64linux -T ./scripts/module-common.lds -T ./arch/arm64/kernel/module.lds --build-id  -o /home/ubuntu/my-LED-task/myled.ko /home/ubuntu/my-LED-task/myled.o /home/ubuntu/my-LED-task/myled.mod.o ;  true
